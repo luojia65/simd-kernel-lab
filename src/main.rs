3ddc8a8723bf99d7e16306e1535adf3f914eda25
luojia65 <me@luojia.cc>
2019-02-11 16:25:59 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-#[no_std]
-#[no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)] 
+#![cfg_attr(test, allow(unused_imports))]
 
-mod vga;
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
